4b27eb0def36046d8b4259957431b2a4330b8812
Jason Zhang <workmail.jason@foxmail.com>
2017-08-24 23:31:46 +0800

    Update AddDigits.java

--- src/easy/AddDigits.java
+++ src/easy/AddDigits.java
@@ -6,14 +6,11 @@ public class AddDigits {
 		if(num==0){
 		return 0;
 		}
-		else return 1+(num-1)%9;
-	        
+		else return 1+(num-1)%9;	        
 	}
 	//一个个找规律
 	 public int addDigits2(int num) {
-
 	        return num==0?0:(num%9==0?9:(num%9));
-
 	    }
 
 	public static void main(String[] args) {
