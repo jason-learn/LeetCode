70b10065d8f3072a9a465a715fa258b45fff6a78
Jason Zhang <workmail.jason@foxmail.com>
2017-08-26 18:44:17 +0800

    Add files via upload

---
+++ src/easy/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package easy;
+
+//70. Climbing Stairs
+public class ClimbingStairs {
+	public int climbingStairs(int n){
+		//找规律发现是fabonacci数列问题
+		if(n==0 || n==1 || n==2) return n;
+		int[] steps = new int[n];
+		steps[0]=1;
+		steps[1]=2;
+		
+		for(int i=2;i<n;i++){
+			steps[i] =  steps[i-1]+steps[i-2];
+		}
+		
+		return steps[n-1];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClimbingStairs c = new ClimbingStairs();
+		System.out.println(c.climbingStairs(4));
+
+	}
+
+}
