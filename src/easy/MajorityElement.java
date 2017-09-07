497df61c0f9ceb3eca34e01f143a258c07bb29f1
Jason Zhang <workmail.jason@foxmail.com>
2017-09-07 10:45:43 +0800

    Update MajorityElement.java

--- src/easy/MajorityElement.java
+++ src/easy/MajorityElement.java
@@ -7,22 +7,22 @@ import java.util.Map;
 public class MajorityElement {
 	 public int majorityElement(int[] nums) {
 		 Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
-		    //Hashtable<Integer, Integer> myMap = new Hashtable<Integer, Integer>();
+		 //Hashtable<Integer, Integer> myMap = new Hashtable<Integer, Integer>();
 		 int output=0;
-	    for (int num: nums) {
-	    	//看是否已经有这个key
-	        if (!myMap.containsKey(num))
-	            myMap.put(num, 1);
-	        else
-	        	//value++
-	            myMap.put(num, myMap.get(num)+1);
-	        //如果大于一半就设为majority
-	        if (myMap.get(num)>nums.length/2) {
-	            output = num;
-	            break;
-	        }
-	    }
-	    return output;
+		 for (int num: nums) {
+			//看是否已经有这个key
+			if (!myMap.containsKey(num))
+			    myMap.put(num, 1);
+			else
+				//value++
+			    myMap.put(num, myMap.get(num)+1);
+			//如果大于一半就设为majority
+			if (myMap.get(num)>nums.length/2) {
+			    output = num;
+			    break;
+			}
+		 }
+		 return output;
 	        
 	 }
 
