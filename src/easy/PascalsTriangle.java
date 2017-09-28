e52cf98d59135b4f2bedef5c7fb3781ca3f12040
Jason Zhang <workmail.jason@foxmail.com>
2017-09-28 15:07:11 +0800

    Add files via upload

---
+++ src/easy/PascalsTriangle.java
@@ -0,0 +1,19 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//118. Pascal's Triangle
+public class PascalsTriangle {
+	public List<List<Integer>> generate(int numRows) {
+		List triangle = new ArrayList();
+		return triangle;
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
